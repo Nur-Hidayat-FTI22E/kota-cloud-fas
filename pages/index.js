b2759e50dad4474dc6e4002203f54b2e5feff4b1
Nur-Hidayat-FTI22E <dayatdj.10jr@gmail.com>
2026-06-12 01:07:35 +0800

    fix: fas fixed redirect

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,12 @@ export default function Home() {
       const response = await fetch('/api/auth', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ hid, fas, originurl })
+        body: JSON.stringify({ 
+          action: 'auth', 
+          hid: hid,
+          fas: fas,
+          originurl: originurl 
+        })
       });
       
       if (response.redirected) {
